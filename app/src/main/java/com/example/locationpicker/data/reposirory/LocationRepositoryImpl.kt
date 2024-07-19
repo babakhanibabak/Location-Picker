81a1a36992e4876530a982cb2937b869aa02bb5c
Babak <babakhanibabak@gmail.com>
2024-07-19 13:02:52 +0330

    create data repositoryImpl

---
+++ app/src/main/java/com/example/locationpicker/data/reposirory/LocationRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package com.example.locationpicker.data.reposirory
+
+import com.example.locationpicker.data.datasource.local.LocationDataProvider
+import com.example.locationpicker.domain.model.LocationListItemModel
+import com.example.locationpicker.domain.repository.LocationRepository
+import javax.inject.Inject
+
+class LocationRepositoryImpl @Inject constructor(
+    private val locationDataProvider: LocationDataProvider
+) : LocationRepository {
+    override suspend fun getLocationList(): List<LocationListItemModel> {
+        return locationDataProvider.getLocationList()
+    }
+
+}
