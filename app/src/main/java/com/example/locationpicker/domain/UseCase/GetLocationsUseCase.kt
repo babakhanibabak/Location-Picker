75796ced85e25c398bb80766a946051d10080f6e
Babak <babakhanibabak@gmail.com>
2024-07-20 12:54:05 +0330

    create LocationUseCase

---
+++ app/src/main/java/com/example/locationpicker/domain/UseCase/GetLocationsUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.locationpicker.domain.UseCase
+
+import com.example.locationpicker.domain.model.LocationListItemModel
+import com.example.locationpicker.domain.repository.LocationRepository
+import javax.inject.Inject
+
+class GetLocationsUseCase @Inject constructor(
+    private val locationRepository: LocationRepository
+) {
+    suspend fun execute() : List<LocationListItemModel> {
+
+     return locationRepository.getLocationList()
+    }
+
+}
