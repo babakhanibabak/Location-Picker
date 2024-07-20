bbfadbe61a6c307f59e585a99219db547dbe23ee
Babak <babakhanibabak@gmail.com>
2024-07-20 13:00:37 +0330

    create LocationListItem and complete LocationListItemModel

--- app/src/main/java/com/example/locationpicker/domain/model/LocationListItemModel.kt
+++ app/src/main/java/com/example/locationpicker/domain/model/LocationListItemModel.kt
@@ -1,6 +1,7 @@
 package com.example.locationpicker.domain.model
 
 data class LocationListItemModel(
+    val name: String,
     val lat: Double,
     val lng: Double
 )
---
+++ app/src/main/java/com/example/locationpicker/ui/locationList/components/LocationListItem.kt
@@ -0,0 +1,15 @@
+package com.example.locationpicker.ui.locationList.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.locationpicker.domain.model.LocationListItemModel
+import com.example.locationpicker.ui.locationList.model.LocationUiModel
+
+@Composable
+fun LocationListItem(
+    modifier: Modifier = Modifier,
+    model: LocationUiModel,
+    onClick: (Double) -> Unit
+) {
+
+}
