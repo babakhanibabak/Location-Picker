9fccd3ab543962a4cc08b229adef95c6fe40eb04
Babak <babakhanibabak@gmail.com>
2024-07-28 17:02:44 +0330

    Continue to complete NavigationScreen

--- app/src/main/java/com/example/locationpicker/ui/navigation/NavigationScreen.kt
+++ app/src/main/java/com/example/locationpicker/ui/navigation/NavigationScreen.kt
@@ -1,23 +1,25 @@
 package com.example.locationpicker.ui.navigation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.BottomAppBarDefaults.windowInsets
-import androidx.compose.material3.BottomAppBarScrollBehavior
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.wear.compose.material.ContentAlpha
+import androidx.wear.compose.material.LocalContentAlpha
 import com.example.locationpicker.ui.theme.LocationPickerTheme
 
 @Composable
@@ -27,25 +29,37 @@ fun NavigationScreen() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun NavigationScreenContent() {
+private fun NavigationScreenContent(
+    onSaveClick: () -> Unit = {}
+) {
     val bottomScrollBehavior = BottomAppBarDefaults.exitAlwaysScrollBehavior()
     Scaffold(modifier = Modifier.fillMaxSize(),
         bottomBar = {
-            BottomAppBar(windowInsets = windowInsets,
+            BottomAppBar(
+                windowInsets = windowInsets,
                 containerColor = Color.LightGray,
-                scrollBehavior = bottomScrollBehavior) {
-BottomBar()
+                scrollBehavior = bottomScrollBehavior
+            ) {
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
+                    BottomBar()
+                }
+
             }
 
         }
     ) { paddingValues ->
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.Bottom
         ) {
 
             // TODO: NavHost here
+
+            MyButtons(modifier = Modifier.padding(15.dp), text = "Save", onClick = onSaveClick)
+
             val navController = rememberNavController()
             NavHost(navController = navController, startDestination = "home") {
                 composable("home") {
