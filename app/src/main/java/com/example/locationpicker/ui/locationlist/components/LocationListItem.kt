5a452965754edcb7465431ff8c25b3d0c6f2af2f
Babak <babakhanibabak@gmail.com>
2024-07-30 20:06:10 +0330

    Continue to complete LocationListItem

--- app/src/main/java/com/example/locationpicker/ui/locationlist/components/LocationListItem.kt
+++ app/src/main/java/com/example/locationpicker/ui/locationlist/components/LocationListItem.kt
@@ -8,8 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,16 +27,23 @@ import com.example.locationpicker.ui.theme.LocationPickerTheme
 fun LocationListItem(
     modifier: Modifier = Modifier,
     model: LocationUiModel,
-    onClick: (LocationUiModel) -> Unit = {}
+    onClick: (LocationUiModel) -> Unit = {},
+    onFavoriteItemClick: () -> Unit = {}
 ) {
     Card(
-        modifier = modifier.padding(8.dp)
+        modifier = modifier
+            .padding(8.dp)
             .fillMaxWidth()
             .border(shape = RoundedCornerShape(4.dp), width = 2.dp, color = Color.Black)
             .clickable { onClick(model) },
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Column(modifier = Modifier.fillMaxSize()) {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.9f)) {
                 androidx.wear.compose.material.Text(
                     modifier = Modifier.fillMaxWidth(),
                     text = model.lat.toString(),
@@ -42,6 +54,9 @@ fun LocationListItem(
                     text = model.lng.toString(),
                     fontSize = 30.sp
                 )
+            }
+            IconButton(onClick = onFavoriteItemClick) {
+                Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = "")
             }
         }
     }
