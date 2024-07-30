6c91e6452d52493f8af96af4b84615b30769ec4e
Babak <babakhanibabak@gmail.com>
2024-07-30 19:44:48 +0330

    Complete FavoriteScreen

--- app/src/main/java/com/example/locationpicker/ui/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/example/locationpicker/ui/favorites/FavoritesScreen.kt
@@ -1,8 +1,75 @@
 package com.example.locationpicker.ui.favorites
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material.Text
+import com.example.locationpicker.ui.theme.LocationPickerTheme
 
 @Composable
 fun FavoritesScreen() {
     // TODO: Implement favorites screen
+    FavoritesScreenContent()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FavoritesScreenContent(
+    modifier: Modifier = Modifier,
+    onBackClick: () -> Unit = {}
+) {
+    Scaffold(modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Favorite Location",
+                        fontSize = 32.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth(),
+                        color = Color.LightGray
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = onBackClick,
+                        colors = IconButtonDefaults.iconButtonColors(Color.Black)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = ""
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(Color.Blue.copy(0.5f))
+            )
+        }) { paddingValues ->
+        Column(modifier = modifier.padding(paddingValues)) {
+
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun FavoritesScreenContentPreview() {
+    LocationPickerTheme {
+        FavoritesScreenContent()
+    }
 }
