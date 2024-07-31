f6d1657bb44fecefd3a01df288b7730c52fc167d
Babak <babakhanibabak@gmail.com>
2024-07-31 14:02:02 +0330

    Continue to complete LocationListItem

--- app/src/main/java/com/example/locationpicker/ui/locationlist/components/LocationListItem.kt
+++ app/src/main/java/com/example/locationpicker/ui/locationlist/components/LocationListItem.kt
@@ -4,16 +4,20 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,7 +32,6 @@ fun LocationListItem(
     modifier: Modifier = Modifier,
     model: LocationUiModel,
     onClick: (LocationUiModel) -> Unit = {},
-    onFavoriteItemClick: () -> Unit = {}
 ) {
     Card(
         modifier = modifier
@@ -37,13 +40,18 @@ fun LocationListItem(
             .border(shape = RoundedCornerShape(4.dp), width = 2.dp, color = Color.Black)
             .clickable { onClick(model) },
     ) {
+        var isFavorite by remember {
+            mutableStateOf(false)
+        }
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Column(modifier = Modifier
-                .fillMaxWidth()
-                .weight(0.9f)) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.9f)
+            ) {
                 androidx.wear.compose.material.Text(
                     modifier = Modifier.fillMaxWidth(),
                     text = model.lat.toString(),
@@ -55,8 +63,12 @@ fun LocationListItem(
                     fontSize = 30.sp
                 )
             }
-            IconButton(onClick = onFavoriteItemClick) {
-                Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = "")
+            IconButton(onClick = { isFavorite = !isFavorite }) {
+                Icon(
+                    imageVector = if (isFavorite) Icons.Filled.Favorite
+                    else Icons.Outlined.FavoriteBorder,
+                    contentDescription = ""
+                )
             }
         }
     }
