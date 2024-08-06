75c3e28853be84c7233cba4739445a85a745a6ba
Babak <babakhanibabak@gmail.com>
2024-08-06 18:02:06 +0330

    Add Customizing Map Controls and Options in mapScreenState

--- app/src/main/java/com/example/locationpicker/ui/map/MapScreenState.kt
+++ app/src/main/java/com/example/locationpicker/ui/map/MapScreenState.kt
@@ -2,10 +2,11 @@ package com.example.locationpicker.ui.map
 
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
 
 data class MapScreenState(
-    val uiSettings: MapUiSettings = MapUiSettings(),
-    val mapProperties: MapProperties = MapProperties(),
+    val uiSettings: MapUiSettings = MapUiSettings(zoomControlsEnabled = true),
+    val mapProperties: MapProperties = MapProperties(mapType = MapType.SATELLITE),
     val currentLocation: LatLng = LatLng(0.0, 0.0),
 )
