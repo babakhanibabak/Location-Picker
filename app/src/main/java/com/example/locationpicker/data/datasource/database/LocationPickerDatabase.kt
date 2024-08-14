10173390357447c5740eaae008ed713855712f55
Babak <babakhanibabak@gmail.com>
2024-08-14 21:23:21 +0330

    Add MIGRATION_1_2 to LocationPickerDatabase

--- app/src/main/java/com/example/locationpicker/data/datasource/database/LocationPickerDatabase.kt
+++ app/src/main/java/com/example/locationpicker/data/datasource/database/LocationPickerDatabase.kt
@@ -2,15 +2,23 @@ package com.example.locationpicker.data.datasource.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.locationpicker.data.datasource.database.dao.LocationDao
 import com.example.locationpicker.data.datasource.database.entity.LocationEntity
 
 @Database(
-    version = 1,
+    version = 2,
     exportSchema = true,
     entities = [LocationEntity::class],
     autoMigrations = [],
 )
 abstract class LocationPickerDatabase : RoomDatabase() {
     abstract fun locationDao(): LocationDao
+}
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        // Perform necessary schema changes here
+        database.execSQL("ALTER TABLE items ADD COLUMN new_column INTEGER NOT NULL DEFAULT 0")
+    }
 }
