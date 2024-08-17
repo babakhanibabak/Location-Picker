0047fc9d388ca2c247ff612504a79ee0dc80d68a
Babak <babakhanibabak@gmail.com>
2024-08-17 19:20:23 +0330

    Edit LocationListScreen

--- app/src/main/java/com/example/locationpicker/ui/locationlist/LocationListScreen.kt
+++ app/src/main/java/com/example/locationpicker/ui/locationlist/LocationListScreen.kt
@@ -2,21 +2,33 @@ package com.example.locationpicker.ui.locationlist
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.room.Room
 import androidx.wear.compose.material.CircularProgressIndicator
 import androidx.wear.compose.material.Text
+import com.example.locationpicker.data.datasource.database.LocationPickerDatabase
 import com.example.locationpicker.ui.locationlist.components.LocationListItem
 import com.example.locationpicker.ui.locationlist.model.LocationListScreenState
 import com.example.locationpicker.ui.locationlist.model.LocationUiModel
@@ -73,17 +85,39 @@ private fun ShowData(
     onItemClick: (String) -> Unit,
     onIconClick: (Boolean) -> Unit
 ) {
+    val context = LocalContext.current
+    val db = Room.databaseBuilder(
+        context,
+        LocationPickerDatabase::class.java, "location"
+    ).fallbackToDestructiveMigration().build()
+    val locations=db.locationDao().getAllLocations()
+
     LazyColumn(
         modifier = Modifier,
         contentPadding = PaddingValues(0.dp),
     ) {
-        items(locationList) {
-            LocationListItem(
-                model = it, onClick = { model -> onItemClick(model.comment) },
-
-                onIconClick = { model -> onIconClick(model.isFavorite) })
+        items(locations) {
+                location ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth().padding(8.dp)
+            ) {
+                IconButton(onClick = {
+                    val updatedLocation = location.copy(isFavorite = !location.isFavorite)
+                    db.locationDao().updateLocation(updatedLocation)
+                }) {
+                    Icon(
+                        imageVector = if (location.isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "Favorite"
+                    )
+                }
+//            LocationListItem(
+//                model = it, onClick = { model -> onItemClick(model.comment) },
+//
+//                onIconClick = { model -> onIconClick(model.isFavorite) })
         }
     }
+}
 }
 
 @Composable
