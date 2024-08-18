076230eafc39f9579c30ff91f7a53ea116d50dec
Babak <babakhanibabak@gmail.com>
2024-08-18 21:42:39 +0330

    Edit FavoriteScreen

--- app/src/main/java/com/example/locationpicker/ui/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/example/locationpicker/ui/favorites/FavoritesScreen.kt
@@ -1,14 +1,26 @@
 package com.example.locationpicker.ui.favorites
 
+import android.location.Location
+import android.media.RouteListingPreference.Item
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.data.EmptyGroup.location
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
 import androidx.wear.compose.material.Text
+import com.example.locationpicker.data.datasource.database.LocationPickerDatabase
+import com.example.locationpicker.data.datasource.database.entity.LocationEntity
+import com.example.locationpicker.ui.locationlist.components.LocationListItem
 import com.example.locationpicker.ui.theme.LocationPickerTheme
 
 @Composable
@@ -20,12 +32,28 @@ fun FavoritesScreen() {
 fun FavoritesScreenContent(
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Favorites Screen", color = Color.Black)
+    val context= LocalContext.current
+    val db= Room.databaseBuilder(
+        context = context,
+        LocationPickerDatabase::class.java, "location"
+
+    ).build()
+    val favoriteItems= remember {
+        mutableSetOf(listOf<LocationEntity>())
+    }
+    LaunchedEffect(key1 = Unit) {
+        favoriteItems.value =db.locationDao().getFavoriteLocations()
+    }
+
+    Column {
+        LazyColumn {
+            items(favoriteItems.value){
+LocationListItem(location ->
+
+    )
+            }
+        }
+
     }
 }
 
